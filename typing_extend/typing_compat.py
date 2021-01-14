f5a1b8492231054669cee7a64249dbc62b94c761
PrettyWood <em.jolibois@gmail.com>
2021-01-14 12:49:05 +0100

    refactor: split `typing_compat` by method/class instead of python version

--- typing_extend/typing_compat.py
+++ typing_extend/typing_compat.py
@@ -3,85 +3,68 @@ Module that handles differences between supported versions of Python
 for some methods / classes of the `typing` module
 """
 import sys
-from typing import Any, Tuple, Type, cast
+from typing import Any, Optional, Tuple, Type, Union, cast
 
 __all__ = ("get_args", "get_origin", "is_typeddict", "TypedDict")
 
-pyver = sys.version_info[:2]
+TypeLike = Union[Type[Any], Union[Any]]
 
 
-def get_args(tp: Type[Any]) -> Tuple[Any, ...]:
-    """For python 3.6 and 3.7, we create a fallback"""
+def get_args(tp: TypeLike) -> Tuple[Any, ...]:
     try:
         from typing import get_args
 
         return get_args(tp)
     except ImportError:
+        # Python 3.6 and 3.7
         return cast(Tuple[Any, ...], getattr(tp, "__args__", ()))
 
 
-# Python 3.10+
-if sys.version_info >= (3, 10):
-    from typing import TypedDict, get_origin, is_typeddict
+def get_origin(tp: TypeLike) -> Optional[TypeLike]:
+    try:
+        from typing import get_origin
 
-# Python 3.9+
-elif sys.version_info >= (3, 9):
-    from typing import Any, Type, get_origin
+        return get_origin(tp)
+    except ImportError:
+        # Python 3.7
+        if sys.version_info[:2] == (3, 7):
+            return getattr(tp, "__origin__", None)
+        # Python 3.6
+        else:
+            assert sys.version_info[:2] == (3, 6)
 
+            from typing import Dict, List, Set, Tuple, Type
+
+            # In python 3.6, the origin of `List[str]` for example
+            # is `List` and not `list`. We hence need an explicit mapping...
+            typing_to_builtin_map = {
+                Dict: dict,
+                List: list,
+                Set: set,
+                Tuple: tuple,
+                Type: type,
+            }
+
+            origin = getattr(tp, "__origin__", None)
+            return typing_to_builtin_map.get(origin, origin)
+
+
+def is_typeddict(tp: TypeLike) -> bool:
+    try:
+        from typing import is_typeddict  # type: ignore[attr-defined]
+
+        return is_typeddict(tp)  # type: ignore[no-any-return]
+    except ImportError:
+        # Python 3.6 to Python 3.9
+        from .utils import lenient_issubclass
+
+        return lenient_issubclass(tp, dict) and hasattr(tp, "__annotations__")
+
+
+if sys.version_info < (3, 9):
+    # Even though `TypedDict` is already in python 3.8,
+    # the class doesn't have `__required_keys__` and `__optional_keys__`,
+    # which prevents a perfect support
     from typing_extensions import TypedDict
-
-    from .utils import lenient_issubclass
-
-    def is_typeddict(tp: Type[Any]) -> bool:
-        return lenient_issubclass(tp, dict) and getattr(tp, "__annotations__", None)
-
-
-# Python 3.8+
-elif sys.version_info >= (3, 8):
-    from typing import Any, Type, get_origin
-
-    from typing_extensions import TypedDict
-
-    from .utils import lenient_issubclass
-
-    def is_typeddict(tp: Type[Any]) -> bool:
-        return lenient_issubclass(tp, dict) and getattr(tp, "__annotations__", None)
-
-
-# Python 3.7
-elif sys.version_info >= (3, 7):
-    from typing import Any, Optional, Tuple, Type
-
-    from typing_extensions import TypedDict
-
-    from .utils import lenient_issubclass
-
-    def get_origin(tp: Type[Any]) -> Optional[Type[Any]]:
-        return getattr(tp, "__origin__", None)
-
-    def is_typeddict(tp: Type[Any]) -> bool:
-        return lenient_issubclass(tp, dict) and getattr(tp, "__annotations__", None)
-
-
-# Python 3.6
 else:
-    from typing import Any, Dict, List, Optional, Set, Tuple, Type
-
-    from typing_extensions import TypedDict
-
-    from .utils import lenient_issubclass
-
-    TYPING_TO_BUILTIN_MAP = {
-        Dict: dict,
-        List: list,
-        Set: set,
-        Tuple: tuple,
-        Type: type,
-    }
-
-    def get_origin(tp: Type[Any]) -> Optional[Type[Any]]:
-        origin = getattr(tp, "__origin__", None)
-        return TYPING_TO_BUILTIN_MAP.get(origin, origin)
-
-    def is_typeddict(tp: Type[Any]) -> bool:
-        return lenient_issubclass(tp, dict) and getattr(tp, "__annotations__", None)
+    from typing import TypedDict
